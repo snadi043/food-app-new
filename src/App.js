4c7fdc423576462768ca9eb13a32968c80ce36fe
snadi043 <snadi043@uottawa.ca>
2023-07-15 22:32:38 -0400

    Initial progress on Meals Feature and corresponding styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
 import './App.module.css';
 import Header from './components/Layout/Header/Header';
+import Meals from './components/Meals/Meals';
 
 const App = () => {
   return (
     <Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
     </Fragment>
   );
 }
